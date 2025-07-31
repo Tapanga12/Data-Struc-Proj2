410d28b49403402fd3d6db99b4c80c91b02990d8
Tapanga Witt <91804327+Tapanga12@users.noreply.github.com>
2025-07-31 14:24:45 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,16 @@
+// Tapanga Witt
+// CIS 2353
+// Summer 2025
+// Project 2
+
+public class Node {
+    int coefficient;
+    int exponent;
+    Node nextNode;
+
+    public Node(int coefficient, int exponent) {
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+        this.nextNode = null;
+    }
+}
